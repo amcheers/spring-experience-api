33a2aa025478c9c1ee7eaa3fbb555068f73a9022
Ahmad Cheers <acheers1@gmail.com>
2026-06-03 19:32:55 -0400

    Initialize Spring Boot app.

---
+++ src/main/java/com/ahmad/experienceapi/ExperienceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.experienceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceApiApplication.class, args);
+	}
+
+}
